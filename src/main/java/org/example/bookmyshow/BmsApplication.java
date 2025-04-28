1b19ede5a4409f7cdfa4af9639a7a45d9b7e66ef
Ajay Sah from TB <ajaysah884@gmail.com>
2025-04-28 15:43:08 +0530

    user sign-up hardcoded in CommandLineRunner run method

--- src/main/java/org/example/bookmyshow/BmsApplication.java
+++ src/main/java/org/example/bookmyshow/BmsApplication.java
@@ -1,5 +1,7 @@
 package org.example.bookmyshow;
 
+import org.example.bookmyshow.controller.UserController;
+import org.example.bookmyshow.dtos.UserSignUpRequestDto;
 import org.example.bookmyshow.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,14 +11,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class BmsApplication implements CommandLineRunner {
-    private UserService userService;
-
+    private UserController userController;
     public static void main(String[] args) {
         SpringApplication.run(BmsApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+        UserSignUpRequestDto userSignUpRequestDto = new UserSignUpRequestDto();
+        userSignUpRequestDto.setEmail("test@gmail.com");
+        userSignUpRequestDto.setPassword("test@123");
 
+        userController.signUp(userSignUpRequestDto);
     }
 }
